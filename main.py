0c81a154e6bcd3d4e4a2af648e4c16fd188a157d
Guhan-3 <146440900+Guhan-3@users.noreply.github.com>
2024-11-13 17:44:58 +0530

    main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import uvicorn
 app = FastAPI(debug=True)
 app.include_router(users.router, prefix="/api/v1/users")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+    #uvicorn.run(app, host="0.0.0.0", port=8000)
