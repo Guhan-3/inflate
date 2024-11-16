0c750958a8c019bab4c29cccf4d31f655e9ade6e
Guhan-3 <146440900+Guhan-3@users.noreply.github.com>
2024-11-16 22:44:33 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from api.v1 import users
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(debug=True)
 app.include_router(users.router, prefix="/api/v1/users")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or specify Flutter's domain
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
